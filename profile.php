689cee9edd5d2640a703e94216b41039f6b81fbb
auzi123 <sukaakuajalah24@gmail.com>
2024-06-12 07:11:24 +0700

    Initial commit

---
+++ profile.php
@@ -0,0 +1,50 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/profile.jpeg" alt="Ahmad Fauzi" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM:</strong></td>
+                        <td align="left" width="50%">2210010510</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Ahmad Fauzi</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Sukamara, 24 Agustus 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0824 2131 2111</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">ahmadfauzii24@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://www.linkedin.com/in/ahmad-fauzi-a83223306?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" style="text-decoration:none;">
+                                <img src="img/linkedin.png" width="30" alt="facebook">
+                            </a>
+                            <a href="https://www.instagram.com/amdfzie" target="_blank" style="text-decoration:none;">
+                                <img src="img/icons8-instagram-48.png" width="30" alt="instagram">
+                            </a>
+                            <a href="https://www.facebook.com/ahamd.fauzi.773?mibextid=ZbWKwL" target="_blank">
+                                <img src="img/facebook.png" width="30" alt="facebok">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
